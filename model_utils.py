415c1eb84c5cacf6dac849a6cf1755563a24fb3d
Joshua Dickey <joshuadickey@gmail.com>
2021-04-25 20:21:16 -0400

    Add files via upload

--- model_utils.py
+++ model_utils.py
@@ -6,6 +6,11 @@ Params, set_logger, save_dict_to_json
 
 import json
 import logging
+import obspy
+from obspy.clients.fdsn import Client
+from scipy.signal import lfilter, butter, decimate, hann
+import numpy as np
+
 #import tensorflow as tf
 #import numpy as np
 #import matplotlib.pyplot as plt
@@ -86,3 +91,58 @@ def save_dict_to_json(d, json_path):
     with open(json_path, 'w') as f:
         d['model_name'] = get_model_name(d)
         json.dump(d, f, indent=4)
+        
+def datetime2epoch(timestamp):
+    return (timestamp - datetime.datetime(1970, 1, 1)).total_seconds()
+
+def get_wav(sta, st_time, en_time, pdict, source):
+    
+    X = []
+    
+    if sta[-2:] == 'AR':
+        sta = sta[:-2] + '31'
+        
+    stream = source.get_waveforms(network="*", location="", station=sta, channel="BH?",
+                             starttime=obspy.UTCDateTime(st_time), endtime=obspy.UTCDateTime(en_time))
+    for cmpt in pdict.cmpts:
+        X.append(stream.select(component=cmpt)[0].data)
+         
+    X = np.stack(X, axis=-1)
+    if X.shape >= (int(pdict.w_len * pdict.s_rate), len(pdict.cmpts)):
+        return X
+    else:
+        raise Exception()
+
+def DAT_normalize(X):
+    X = X - np.expand_dims(np.mean(X,1),1)
+    X = X / (np.expand_dims(np.expand_dims(np.abs(X).max(1).max(1), 1), 1) + .001)
+    return X
+
+def butter_bandpass(lowcut, highcut, fs, order=8):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def DAT_filter(X, pdict, order=3):
+    lowcut = pdict.f_low
+    highcut = pdict.f_high
+    fs = pdict.s_rate
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    X_filt = lfilter(b, a, X, axis=1)
+    
+
+    if pdict.zp is True:
+        X_filt = lfilter(b, a, X_filt[:,::-1,:])[::-1]
+        
+    return X_filt 
+
+def DAT_taper(X, taper_percentage=.1):
+    npts = X.shape[1]
+    taper_len = int(npts * taper_percentage)
+    taper_sides = hann(2 * taper_len + 1)
+    taper = np.hstack((taper_sides[:taper_len], np.ones(npts - taper_len)))
+    return X * np.reshape(taper,(1,-1,1))
+
